75e588300039ebe2187492ce23f3ba7d1b9f016b
Mohammed Alani <34628814+MohammaedAlani@users.noreply.github.com>
2025-06-18 01:39:58 +0300

    fixde

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'last_login_at'
     ];
 
     /**
@@ -44,6 +46,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'last_login_at' => 'datetime',
         ];
     }
 }
