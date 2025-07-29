f1b549f194443e24d970000af5f7c8253848f057
Mohammed Alani <34628814+MohammaedAlani@users.noreply.github.com>
2025-07-30 01:41:00 +0300

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Schedule;
 
 // call this export:customers-mail every start day
-Schedule::command('export:customers-mail')->dailyAt('01:39');
+Schedule::command('export:customers-mail')->dailyAt('00:01');
