7a46e6dc00ffe566a7d348df29be31c2eb66cb38
Mohammed Alani <34628814+MohammaedAlani@users.noreply.github.com>
2025-07-30 20:12:35 +0300

    Update TransactionController.php

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -10,7 +10,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Maatwebsite\Excel\Excel;
+use Maatwebsite\Excel\Facades\Excel;
 
 class TransactionController extends Controller
 {
